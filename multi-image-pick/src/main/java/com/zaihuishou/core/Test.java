d3c75dbf0cfa80fa1788b66dfbc4f4c23f8ad714
tanzhiqiang <tanzhiqiang.cathy@gmail.com>
2015-12-03 09:55:32 +0800

    push success

---
+++ multi-image-pick/src/main/java/com/zaihuishou/core/Test.java
@@ -0,0 +1,13 @@
+package com.zaihuishou.core;
+
+import android.util.Log;
+
+/**
+ * Created by zhiqiang on 15-12-1.
+ */
+public class Test {
+    public static final String TAG = "zaihuishou";
+    public static void testLog() {
+        Log.i(TAG,"test jcenter");
+    }
+}
